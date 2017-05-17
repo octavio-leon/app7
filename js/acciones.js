7375d3eb9a45e37e20388abf8fc9513d7dafbf52
octavio-leon <octavioleonamador@gmail.com>
2017-05-17 07:33:29 -0500

    subida3

--- js/acciones.js
+++ js/acciones.js
@@ -1,1 +1,34 @@
 // JavaScript Document
+
+$(document).ready(function(e)  {
+	document.addEventListener("deviceready", function(){
+		
+	$('#izquierda').on("swipeleft",function(){
+		navigator.notification.alert("Deslizo a la izquierda", function(){"Aplicacion7","Aceptar"});
+});
+
+$('derecha').on("swiperight",function(){
+	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.vibrate(1000);
+			break;
+			
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+		}
+	},"Aplicacion8","Beep,vibrar,Cancelar");
+	
+});
+	},false);
+});
+	
+	
+	
+	
+	
+	
+	
+	
